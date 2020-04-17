226832a27a2b524dae47967da2ba57549ee549b6
unknown <admin@cryptostat.org>
2020-04-17 18:08:07 +1000

    add config file, and emit to website

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+process.title = 'Google 2FA'
 const dotenv = require('dotenv').config()
 const speakeasy = require('speakeasy')
 const chalk = require('chalk')
-const log = console.log
+const path = require('path');
+const fs = require('fs')
 
-process.title = 'Google 2FA'
+const express = require('express')
+const helmet = require('helmet')
+const socket = require('socket.io')
+
+const log = console.log
+const app = express()
+const port = process.env.SERVER_PORT || 6060
+
+const server = app.listen(port, () => {
+  app.use(express.json())
+  app.use(helmet())
+  app.use('/', (request, response) => {
+    response.sendFile(path.join(__dirname + '/index.html'));
+  })
+  console.log(`Listening on port: ${port}`)
+})
+
+const io = socket(server);
 
 function getTwoFactor(secret) {
   return new Promise((resolve, reject) => {
@@ -13,19 +32,31 @@ function getTwoFactor(secret) {
     });
     resolve(token);
   })
-
 }
 
+ready = false
+keys = []
 
-setInterval(()=>{
-  process.stdout.write("\u001b[2J\u001b[0;0H")
-  console.log(`---- 2FA ----`)
-  getTwoFactor(process.env.MEX)
-    .then(token => {
-      log(`${chalk.cyan('Bitmex')}\t${token}`)
+const secrets = require('./secrets.json')
+
+setInterval(() => {
+  let promises = []
+  secrets.forEach((secret) => {
+    promises.push(getTwoFactor(secret.secret))
+  })
+  Promise.all(promises)
+  .then((data) => {
+    process.stdout.write("\u001b[2J\u001b[0;0H")
+    let payload = []
+    data.forEach((result, i) => {
+      payload.push({
+        label: secrets[i].website,
+        email: secrets[i].email,
+        code: result
+      })
+      log(`${chalk.cyan(secrets[i].website)}\t${result}`)
     })
-    getTwoFactor(process.env.BNB)
-    .then(token => {
-      log(`${chalk.cyan('Binance')}\t${token}`)
-    })
-  },1000)
+    io.emit('2fa', payload)
+  })
+
+}, 1000)
